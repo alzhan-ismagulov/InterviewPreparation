3ef0913e3972650f88a83b5aaa1fd490460cc237
alzhan <aismagulov@gmail.com>
2020-02-15 18:13:57 +0100

    SymmetricTree

--- src/TreesAndGraphs/SymmetricTree.java
+++ src/TreesAndGraphs/SymmetricTree.java
@@ -1,5 +1,5 @@
 package TreesAndGraphs;
-
+//Approach 1: Recursive
 public class SymmetricTree {
     public boolean isSymmetric(TreeNode root) {
         return isMirror(root, root);
@@ -11,5 +11,23 @@ public class SymmetricTree {
         return (t1.val == t2.val)
                 && isMirror(t1.right, t2.left)
                 && isMirror(t1.left, t2.right);
+    }
+//Approach 2: Iterative
+    public boolean isSymmetric2(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            if (t1 == null && t2 == null) continue;
+            if (t1 == null || t2 == null) return false;
+            if (t1.val != t2.val) return false;
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t1.right);
+            q.add(t2.left);
+        }
+        return true;
     }
 }
