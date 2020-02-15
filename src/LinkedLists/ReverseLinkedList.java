9f11bf3e7bd3f5bfb4ebd1f94239910c1790d6bb
alzhan <aismagulov@gmail.com>
2020-02-15 17:51:55 +0100

    ReverseLinkedList

---
+++ src/LinkedLists/ReverseLinkedList.java
@@ -0,0 +1,23 @@
+package LinkedLists;
+
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    public ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+}
